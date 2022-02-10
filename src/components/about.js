5dbedbe81ab1cc71a60e0dc294c3dcb852e316a6
AyushiSoumya <98179985+AyushiSoumya@users.noreply.github.com>
2022-02-11 00:55:09 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -3,11 +3,7 @@ import "./about.scss"
 
 export default function About() {
     return (
-<<<<<<< HEAD
-        <p>hello blah</p>
-=======
 
-        <p>hello faraz</p>
->>>>>>> 95590b37e729bd7525f4055b7fb319a74c3a0423
+        <p>hello blah</p>
     )
 }
