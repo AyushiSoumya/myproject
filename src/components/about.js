1b9401520228b44e0de1bfb467eb0be6f12ef611
farazayub <77578758+farazayub@users.noreply.github.com>
2022-02-11 00:41:09 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,14 @@ import "./about.scss"
 
 export default function About() {
     return (
+<<<<<<< Updated upstream
         <p>hello faraz</p>
+=======
+<<<<<<< HEAD
+        <p>hello Faraz</p>
+=======
+        <p>hello ayushi</p>
+>>>>>>> 97b0e30ab79b84c1df1b910502eab52480db71ca
+>>>>>>> Stashed changes
     )
 }
