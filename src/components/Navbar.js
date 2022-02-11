e2b790c46fa3df35ca2f02b2bcb0d202d4e5b69a
farazayub <77578758+farazayub@users.noreply.github.com>
2022-02-11 14:20:40 +0530

    problems with navbar fixed ->1st

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,17 +10,18 @@ export default function Navbar(props) {
     return (<>
         < nav className="navbar nav navbar-expand-lg navbar-light bg-light" id="Nav">
             <div className="container-fluid" id="NavMain">
-
-                <a className="navbar-brand" href="/"> <img src={logo} id="logo" />  </a>
-                <div id="quote">
+                <div>
+                <a className="navbar-brand" href="/"> <img src={logo} id="logo" /></a>
+                </div>
+                <div className='container-fluid justify-content-center' id="quote">
                     <h2>{props.quote}</h2>
                 </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" id="options">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav mb-2 mb-lg-0" id="options">
 
                         <li className="nav-item">
 
@@ -49,8 +50,8 @@ export default function Navbar(props) {
                         </li>
 
                     </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <form className="d-flex" id='search-container'>
+                        <input id='search-box' className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit" id="btn">Search</button>
                     </form>
                 </div>
